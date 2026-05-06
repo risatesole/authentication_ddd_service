bad7f17460cf0a1dc57c6b2b5ebd54d4912342a2
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-05-06 09:49:10 -0400

    chore: fix indenting in email value object

--- src/domain/value_objects/Email.ts
+++ src/domain/value_objects/Email.ts
@@ -1,6 +1,6 @@
 class Email {
-    email:string = "";
-    constructor(email:string){
-    this.email = email;            
-    }
+  email: string = "";
+  constructor(email: string) {
+    this.email = email;
+  }
 }
